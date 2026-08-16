b7124ca1b2c6773c3adaf28ca6f9f8e57fba6668
rektide <rektide+scm@voodoowarez.com>
2026-08-16 05:00:43 -0400

    Expose query operations to application consumers

--- packages/query-kysely/src/index.ts
+++ packages/query-kysely/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./domain/index.ts";
+export * from "./direct/index.ts";
+export * from "./operations/index.ts";
 export * from "./query/index.ts";
 export * from "./relations/index.ts";
 export * from "./runtime/index.ts";
